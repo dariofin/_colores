2cb5752f560b5ab4810afa816b8e451bb89f700b
Darío Finelli <61764276+dariofin@users.noreply.github.com>
2023-12-06 03:51:02 -0300

    Agregado el boton "copiar" aún sin función

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const body = document.getElementsByTagName("body");
 const titulo = document.getElementById("titulo");
 const colorHexNum = document.getElementById("colorhex-num");
 const botonCambiar = document.getElementById("boton-cambiar");
+const botonCopy = document.getElementById("copy-button");
 
 const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 // console.log((body[0].style.backgroundColor = "red"));
 
 botonCambiar.addEventListener("click", fnCambiaFondo);
+botonCopy.addEventListener("click", (e) => alert("copiado"));
 
 function fnCambiaFondo() {
 	const colorRndm = `#${fnRandom()}`;
